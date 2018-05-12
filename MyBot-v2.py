8edf0a3f04ffc3e5cde45c3aa39737971e40d148
Rahul Barhate <rahulbarhate97@gmail.com>
2018-05-13 01:35:45 +0530

    First edit
    Doubled the speed and made sure that all the ships do not go to the same planet.

---
+++ MyBot-v2.py
@@ -0,0 +1,64 @@
+"""
+Welcome to your first Halite-II bot!
+
+This bot's name is Settler. It's purpose is simple (don't expect it to win complex games :) ):
+1. Initialize game
+2. If a ship is not docked and there are unowned planets
+2.a. Try to Dock in the planet if close enough
+2.b If not, go towards the planet
+
+Note: Please do not place print statements here as they are used to communicate with the Halite engine. If you need
+to log anything use the logging module.
+"""
+# Let's start by importing the Halite Starter Kit so we can interface with the Halite engine
+import hlt
+# Then let's import the logging module so we can print out information
+import logging
+
+# GAME START
+# Here we define the bot's name as Settler and initialize the game, including communication with the Halite engine.
+game = hlt.Game("Settler-v2")
+# Then we print our start message to the logs
+logging.info("Starting my Settler bot!")
+
+planned_planets = []
+
+while True:
+    # TURN START
+    # Update the map for the new turn and get the latest version
+    game_map = game.update_map()
+
+    # Here we define the set of commands to be sent to the Halite engine at the end of the turn
+    command_queue = []
+    # For every ship that I control
+    for ship in game_map.get_me().all_ships():
+        # If the ship is docked
+        if ship.docking_status != ship.DockingStatus.UNDOCKED:
+            # Skip this ship
+            continue
+
+        # For each planet in the game (only non-destroyed planets are included)
+        for planet in game_map.all_planets():
+            # If the planet is owned
+            if planet.is_owned():
+                # Skip this planet
+                continue
+
+            # If we can dock, let's (try to) dock. If two ships try to dock at once, neither will be able to.
+            if ship.can_dock(planet):
+                # We add the command by appending it to the command_queue
+                command_queue.append(ship.dock(planet))
+            else:
+                if planet in planned_planets:
+                    continue
+                else:               
+                    navigate_command = ship.navigate(ship.closest_point_to(planet), game_map, speed=int(hlt.constants.MAX_SPEED), ignore_ships=True)
+                    if navigate_command:
+                        command_queue.append(navigate_command)
+                        planned_planets.append(planet)
+            break
+
+    # Send our set of commands to the Halite engine for this turn
+    game.send_command_queue(command_queue)
+    # TURN END
+# GAME END
